770915e394bb297e3c9a7e9d2931b7db18b78615
Vishal Pawar <vishalpawar425@gmail.com>
2022-09-18 22:27:22 +0530

    Used hooks

--- app2/src/index.js
+++ app2/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import SearchParams from './SearchParams';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <SearchParams />
   </React.StrictMode>
 );
 
